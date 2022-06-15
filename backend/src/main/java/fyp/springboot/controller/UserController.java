99ac7abf3ce7d4c53ac15799352386e964ce51e3
4F12 Concuss <93592987+max4212@users.noreply.github.com>
2022-06-15 18:25:12 +0800

    Update UserController.java

--- backend/src/main/java/fyp/springboot/controller/UserController.java
+++ backend/src/main/java/fyp/springboot/controller/UserController.java
@@ -1,92 +1,72 @@
-package fyp.springboot.controller;
+package fyp.evoting.backend.controller;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import fyp.springboot.exception.ResourceNotFoundException;
-import fyp.springboot.entity.User;
-import fyp.springboot.repository.UserRepository;
+import fyp.evoting.backend.exception.ResourceNotFoundException;
+import fyp.evoting.backend.model.User;
+import fyp.evoting.backend.repository.UserRepository;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/")
 public class UserController {
 
-  @Autowired
-  UserRepository userRepository;
-
-  @GetMapping("/users")
-  public ResponseEntity<List<User>> getAllUsers(@RequestParam(required = false) String userName) {
-    List<User> users = new ArrayList<User>();
-
-    if (userName == null)
-      userRepository.findAll().forEach(users::add);
-    else
-      userRepository.findByUserNameContaining(userName).forEach(users::add);
-
-    if (users.isEmpty()) {
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-
-    return new ResponseEntity<>(users, HttpStatus.OK);
-  }
-
-  @GetMapping("/users/{id}")
-  public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
-    User user = userRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("User " + id + " Not Found"));
-
-    return new ResponseEntity<>(user, HttpStatus.OK);
-  }
-
-  @PostMapping("/users/{id}")
-  public ResponseEntity<User> addUser(@RequestBody User user) {
-    User _user = userRepository.save(new User(user.getPassword(), user.getUserName(), user.getEmail(), 0));
-    return new ResponseEntity<>(_user, HttpStatus.CREATED);
-  }
-
-  @PutMapping("/users/{id}")
-  public ResponseEntity<User> editUser(@PathVariable("id") long id, @RequestBody User user) {
-    User _user = userRepository.findById(id)
-        .orElseThrow(() -> new ResourceNotFoundException("User " + id + " Not Found"));
-
-    _user.setPassword(user.getPassword());
-    _user.setUserName(user.getUserName());
-    _user.setEmail(user.getEmail());
-    _user.setUserType(user.initUserType());
-    
-    return new ResponseEntity<>(userRepository.save(_user), HttpStatus.OK);
-  }
-
-  @DeleteMapping("/users/{id}")
-  public ResponseEntity<HttpStatus> deleteUser(@PathVariable("id") long id) {
-    userRepository.deleteById(id);
-    
-    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-  }
-
-  @GetMapping("/users/0")
-  public ResponseEntity<List<User>> findByUserType() {
-    List<User> users = userRepository.findByUserType(0);
-
-    if (users.isEmpty()) {
-      return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
-    
-    return new ResponseEntity<>(users, HttpStatus.OK);
-  }
+	@Autowired
+	private UserRepository userRepository;
+	
+	// get all users
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userRepository.findAll();
+	}		
+	
+	// create user rest api
+	@PostMapping("/users")
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+	
+	// get user by id rest api
+	@GetMapping("/users/{id}")
+	public ResponseEntity<User> getUserById(@PathVariable Long id) {
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("User not exist with id :" + id));
+		return ResponseEntity.ok(user);
+	}
+	
+	// update user rest api
+	
+	@PutMapping("/users/{id}")
+	public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User userDetails){
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("User not exist with id :" + id));
+		
+		user.setUserName(userDetails.getUserName());
+		user.setUserPW(userDetails.getUserPW());
+		user.setEmail(userDetails.getEmail());
+//		user.setUserType(userDetails.getUserType());
+		
+		User updatedUser = userRepository.save(user);
+		return ResponseEntity.ok(updatedUser);
+	}
+	
+	// delete user rest api
+	@DeleteMapping("/users/{id}")
+	public ResponseEntity<Map<String, Boolean>> deleteUser(@PathVariable Long id){
+		User user = userRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("User not exist with id :" + id));
+		
+		userRepository.delete(user);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+	
+	
 }
