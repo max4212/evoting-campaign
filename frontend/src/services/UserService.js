d3ff5c79d9ae8919fece250ef54ae04685c45ef3
4F12 Concuss <93592987+max4212@users.noreply.github.com>
2022-06-15 18:22:16 +0800

    Update UserService.js

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = "http://localhost:8080/api/users";
+const USER_API_BASE_URL = "http://localhost:8080/api/v1/users";
 
 class UserService {
 
@@ -8,20 +8,20 @@ class UserService {
         return axios.get(USER_API_BASE_URL);
     }
 
-    addUser(user){
+    createUser(user){
         return axios.post(USER_API_BASE_URL, user);
     }
 
-    getUserById(id){
-        return axios.get(USER_API_BASE_URL + '/' + id);
+    getUserById(userId){
+        return axios.get(USER_API_BASE_URL + '/' + userId);
     }
 
-    editUser(user, id){
-        return axios.put(USER_API_BASE_URL + '/' + id, user);
+    updateUser(user, userId){
+        return axios.put(USER_API_BASE_URL + '/' + userId, user);
     }
 
-    deleteUser(id){
-        return axios.delete(USER_API_BASE_URL + '/' + id);
+    deleteUser(userId){
+        return axios.delete(USER_API_BASE_URL + '/' + userId);
     }
 }
 
