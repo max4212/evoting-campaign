5a638fc3ed88d9253bd5d507a147ac6e09de13a8
4F12 Concuss <93592987+max4212@users.noreply.github.com>
2022-06-15 18:25:41 +0800

    Update Campaign.java

--- backend/src/main/java/fyp/springboot/entity/Campaign.java
+++ backend/src/main/java/fyp/springboot/entity/Campaign.java
@@ -1,91 +1,75 @@
-package fyp.springboot.entity;
+package fyp.evoting.backend.model;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.beans.factory.annotation.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.util.*;
 
 @Entity
-@Table(name = "campaigns")
+@Table(name="campaigns")
 public class Campaign {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-
-  @Column(name = "campaignName")
-  @NotNull(message = "Please Enter A Campaign Name")
-  private String campaignName;
-
-  @Column(name = "deadline")
-  private Date deadline;
-  
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "user_id", nullable = false)
-  @OnDelete(action = OnDeleteAction.CASCADE)
-  @JsonIgnore
-  private User user;
-
-  @Column(name = "status")
-  @Value("${some.key:0}")
-  private int status;
-
-  public Campaign() {
-
-  }
-
-  public Campaign(String campaignName, Date deadline, User user, int status) {
-    this.campaignName = campaignName;
-    this.deadline = deadline;
-    this.user = user;
-    this.status = status;
-  }
-
-  public long getCampaignId() {
-    return id;
-  }
-
-  public String getCampaignName() {
-    return campaignName;
-  }
-
-  public void setCampaignName(String campaignName) {
-    this.campaignName = campaignName;
-  }
-
-  public Date getDeadline() {
-    return deadline;
-  }
-
-  public void setDeadline(Date deadline) {
-    this.deadline = deadline;
-  }
-
-  public User getUser() {
-    return user;
-  }
-
-  public void setUser(User user) {
-    this.user = user;
-  }
-
-  public int initStatus() {
-    return status = 0;
-  }
-
-  public void setStatus(int initStatus) {
-    this.status = initStatus;
-  }
-
-  @Override
-  public String toString() {
-    return "Campaign [id=" + id + ", campaignName=" + campaignName + ", deadline=" + deadline + ", status=" + status + "]";
-  }
-
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(name="campaign_name", nullable = false)
+	private String campaignName;
+	@Column(name="closing_date", nullable = false)	
+	private Date deadline;
+	@Column(name="campaign_status")
+	@Enumerated(EnumType.ORDINAL)
+    private CampaignStatus campaignStatus = CampaignStatus.Open;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "tutorial_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	User user;
+	
+	public Campaign() {
+		
+	}
+	
+	public Campaign(String campaignName, Date deadline, CampaignStatus campaignStatus, User user) {
+		super();
+		this.campaignName = campaignName;
+		this.deadline = deadline;
+		this.campaignStatus = campaignStatus;
+		this.user = user;	
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCampaignName() {
+		return campaignName;
+	}
+	public void setCampaignName(String campaignName) {
+		this.campaignName = campaignName;
+	}
+	public Date getDeadline() {
+		return deadline;
+	}
+	public void setDeadline(Date deadline) {
+		this.deadline = deadline;
+	}
+	public CampaignStatus getCampaignStatus() {
+		return campaignStatus;
+	}
+	public void setCampaignStatus(CampaignStatus campaignStatus) {
+		this.campaignStatus = campaignStatus;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
 }
