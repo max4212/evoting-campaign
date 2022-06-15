1b7d7ec3ab6108332dc4fb5d46695cb763c80207
4F12 Concuss <93592987+max4212@users.noreply.github.com>
2022-06-15 18:26:08 +0800

    Update Option.java

--- backend/src/main/java/fyp/springboot/entity/Option.java
+++ backend/src/main/java/fyp/springboot/entity/Option.java
@@ -1,77 +1,54 @@
-package fyp.springboot.entity;
+package fyp.evoting.backend.model;
 
 import javax.persistence.*;
-import javax.validation.constraints.*;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.beans.factory.annotation.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
-@Table(name = "options")
+@Table(name="options")
 public class Option {
+	@Id
+	@Column(name="option_id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(name="option_desc", nullable = false)
+	private String optionDesc;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "tutorial_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIgnore
+	Campaign campaign;
 	
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-
-  @Column(name = "option")
-  @NotNull(message = "Please Enter Option")
-  private String option;
-
-  @ManyToOne(fetch = FetchType.LAZY, optional = false)
-  @JoinColumn(name = "campaign_id", nullable = false)
-  @OnDelete(action = OnDeleteAction.CASCADE)
-  @JsonIgnore
-  private Campaign campaign;
-
-  @Column(name = "count")
-  @Value("${some.key:0}")
-  private int count;
-  
-  public Option() {
-	  
-  }
-  
-  public Option(String option, Campaign campaign, int count) {
-	this.option = option;
-	this.campaign = campaign;
-	this.count = count;
-}
-
-public Long getOptionId() {
-    return id;
-  }
-
-  public String getOption() {
-    return option;
-  }
-
-  public void setOption(String option) {
-    this.option = option;
-  }
-
-  public Campaign getCampaign() {
-    return campaign;
-  }
-
-  public void setCampaign(Campaign campaign) {
-    this.campaign = campaign;
-  }
-
-  public int initCount() {
-	    return count = 0;
-  }
-
-  public void setCount(int initCount) {
-    this.count = initCount;
-  }
-  
-  @Override
-  public String toString() {
-    return "Option [id=" + id + ", option=" + option + ", campaign=" + campaign + ", count=" + count + "]";
-  }
-  
+	public Option() {
+		
+	}
+	
+	public Option(String optionDesc, Campaign campaign) {
+		super();
+		this.optionDesc = optionDesc;
+		this.campaign = campaign;	
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getOptionDesc() {
+		return optionDesc;
+	}
+	public void setOptionDesc(String optionDesc) {
+		this.optionDesc = optionDesc;
+	}
+	public Campaign getCampaign() {
+		return campaign;
+	}
+	public void setCampaign(Campaign campaign) {
+		this.campaign = campaign;
+	}
+	
 }
